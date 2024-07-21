926daed3f8b9aff295980acecdeb8deffc372d75
yanntafou <yanntafou@gmail.com>
2024-07-21 10:46:57 -0400

    Update UserManagementController.java

--- src/main/java/com/yano/smartshop/controllers/UserManagementController.java
+++ src/main/java/com/yano/smartshop/controllers/UserManagementController.java
@@ -1,6 +1,7 @@
 package com.yano.smartshop.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
