3d49e36379d75fd900abedc54d101562f5370389
yanntafou <yanntafou@gmail.com>
2024-07-25 02:28:06 -0400

    Update UserService.js

--- src/components/service/UserService.js
+++ src/components/service/UserService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 class UserService{
     
-    static BASE_URL = "https://www.yanofolio.ca"
+    static BASE_URL = "postgresql://postgresql.yanofolio.ca:5732/postgres"
 
     static async login(email, password){
         try{
